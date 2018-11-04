92bc49cfde61aa1969384fb706697eb98b2fea1d
Swizec Teller <swizec@swizec.com>
2018-11-04 13:49:51 -0800

    Initial version and demo to prove it works

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,20 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import Barchart from "./Barchart";
+import "./style.css";
 
 class Demo extends Component {
-  render() {
-    return <div>
-      <h1>d3blackbox Demo</h1>
-      <Example/>
-    </div>
-  }
+    render() {
+        return (
+            <div>
+                <h1>d3blackbox Demo</h1>
+                <svg width="800" height="600">
+                    <Barchart x={10} y={10} width={300} height={200} />
+                </svg>
+            </div>
+        );
+    }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+export default function D3blackbox(D3render) {
+    return class Blackbox extends React.Component {
+        anchor = React.createRef();
+
+        componentDidMount() {
+            D3render(this.anchor, this.props, this.state);
+        }
+        componentDidUpdate() {
+            D3render(this.anchor, this.props, this.state);
+        }
+
+        render() {
+            const { x, y } = this.props;
+            return <g transform={`translate(${x}, ${y})`} ref={this.anchor} />;
+        }
+    };
 }
