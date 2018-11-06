b0193203ee9d2b1abc0ee0bd93901150e5b732f9
Swizec Teller <swizec@swizec.com>
2018-11-05 23:50:07 -0800

    Add documentation

--- README.md
+++ README.md
@@ -1,16 +1,65 @@
 # d3blackbox
 
-[![Travis][build-badge]][build]
 [![npm package][npm-badge]][npm]
-[![Coveralls][coveralls-badge]][coveralls]
 
-Describe d3blackbox here.
+Take any D3 example you find in the wild and wrap it in a React component. Great for quick experiments and meeting deadlines. 😛
 
-[build-badge]: https://img.shields.io/travis/user/repo/master.png?style=flat-square
-[build]: https://travis-ci.org/user/repo
+## Meet your deadline in 2 minutes
+
+Check out how it works in [a live Codesandbox](https://codesandbox.io/s/9jm82v2lry)
+
+1. Install `d3blackbox` with npm
+
+```
+$ npm install -s d3blackbox
+```
+
+2. Find a D3 example you like
+3. Copy its code, put it in a function
+4. Wrap function in `D3blackbox`
+
+```javascript
+import React from "react";
+import D3blackbox from "d3blackbox";
+import * as d3 from "d3";
+
+const MyDataviz = D3blackbox(function(anchor, props, state) {
+    const svg = d3.select(anchor.current);
+
+    // the rest of your D3 code
+});
+
+export default MyDataviz;
+```
+
+5. Render inside an `<svg></svg>` element
+6. Enjoy your blackbox D3 component
+
+D3blackbox renders an anchor element and delegates control to your render function. You get an `anchor` ref, the component's `props`, and `state`. Do what you want :)
+
+Great for meeting deadlines and playing around with other people's code. Not recommended for large scale use due to performance constraints. Your render function runs on every component update and redraws the entire DOM subtree. React's rendering engine can't help you.
+
+That's why it's called a sandbox.
+
+## What if I don't use D3?
+
+That's okay. `D3blackbox` lets you delegate control to any rendering library you want. As long as you're okay rendering into a `<g></g>` element.
+
+You can even use this approach to render Vue apps inside your React apps. 🤨
+
+## What if I don't want `<g>`?
+
+That's okay you weirdo. Use the `component` argument to specify a different component.
+
+```javascript
+render() {
+    return <MyDataviz component="div" />
+}
+```
+
+## HOCs are so September, do you have hooks?
+
+Yes.
 
 [npm-badge]: https://img.shields.io/npm/v/npm-package.png?style=flat-square
 [npm]: https://www.npmjs.org/package/npm-package
-
-[coveralls-badge]: https://img.shields.io/coveralls/user/repo/master.png?style=flat-square
-[coveralls]: https://coveralls.io/github/user/repo
--- demo/src/index.js
+++ demo/src/index.js
@@ -9,9 +9,91 @@ class Demo extends Component {
         return (
             <div>
                 <h1>d3blackbox Demo</h1>
-                <svg width="800" height="600">
+
+                <p>
+                    Take any D3 example you find in the wild and wrap it in a
+                    React component. Great for quick experiments and meeting
+                    deadlines. 😛
+                </p>
+
+                <p>
+                    <a href="https://github.com/Swizec/d3blackbox">
+                        Source code on GitHub
+                    </a>
+                </p>
+
+                <h2>Basic usage</h2>
+
+                <p>
+                    A{" "}
+                    <a href="https://cdn.rawgit.com/mbostock/3885304/raw/a91f37f5f4b43269df3dbabcda0090310c05285d/index.html">
+                        barchart from D3
+                    </a>{" "}
+                    docs wrapped in a React component. Takes about 2 minutes to
+                    do from scratch.
+                </p>
+
+                <svg width="800" height="250">
                     <Barchart x={10} y={10} width={300} height={200} />
                 </svg>
+
+                <h3>
+                    1. Install <code>d3blackbox</code> and <code>d3</code>
+                </h3>
+                <p>
+                    <pre>$ npm install d3blackbox</pre>
+                </p>
+                <h3>
+                    2. Wrap D3 code in <code>D3blackbox</code>
+                </h3>
+                <p>
+                    <pre>
+                        {`
+import React from "react";
+import D3blackbox from "d3blackbox";
+import * as d3 from "d3";
+
+const Barchart = D3blackbox(function(anchor, props, state) {
+    const svg = d3.select(anchor.current);
+
+    // the rest of your D3 code
+});
+
+export default Barchart;`}
+                    </pre>
+                </p>
+                <h3>3. Render your component</h3>
+                <p>
+                    <pre>{`
+<Barchart x={10} y={10} width={300} height={200} />
+                    `}</pre>
+                </p>
+                <p>
+                    <svg width="800" height="250">
+                        <Barchart x={10} y={10} width={300} height={200} />
+                    </svg>
+                </p>
+
+                <h2>Use different anchor component</h2>
+                <p>
+                    By default D3blackbox wraps your D3 render method in a
+                    &lt;g&gt; element. You can change that with the{" "}
+                    <code>component</code> prop.
+                </p>
+                <p>
+                    <pre>{`
+<Barchart componenet="div" />
+                    `}</pre>
+                </p>
+                <p>Make sure your renderer knows how to handle that.</p>
+                <p />
+
+                <h2>Use with React hooks</h2>
+
+                <p>
+                    Built by <a href="https://swize.com">Swizec</a>,<br />
+                    Cheers ❤️
+                </p>
             </div>
         );
     }
